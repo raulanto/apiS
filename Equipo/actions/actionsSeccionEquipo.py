c9d1e9d729a904c9c8bf1089a404d3898646e253
raul <74162376+raulanto@users.noreply.github.com>
2024-09-15 00:07:35 -0600

    reportlab elimn

--- Equipo/actions/actionsSeccionEquipo.py
+++ Equipo/actions/actionsSeccionEquipo.py
@@ -1,8 +1,5 @@
 from django.http import HttpResponse
 import csv
-# from reportlab.lib import colors
-# from reportlab.lib.pagesizes import letter
-# from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
 
 def exportSeccionEquipo_csv(modeladmin, request, queryset):
     response = HttpResponse(content_type='text/csv')
@@ -17,27 +14,3 @@ def exportSeccionEquipo_csv(modeladmin, request, queryset):
 
 exportSeccionEquipo_csv.short_description = "Exportar como CSV"
 
-# def export_pdf(modeladmin, request, queryset):
-#     response = HttpResponse(content_type='application/pdf')
-#     response['Content-Disposition'] = 'attachment; filename="secciones.pdf"'
-#
-#     doc = SimpleDocTemplate(response, pagesize=letter)
-#     table_data = [['ID', 'Nombre', 'Usuario', 'Equipo']]
-#
-#     for obj in queryset:
-#         table_data.append([obj.id, obj.nombre, obj.fkequipo.usuario.username if obj.fkequipo and obj.fkequipo.usuario else None, obj.fkequipo.nombre if obj.fkequipo else None])
-#
-#     table = Table(table_data)
-#     style = TableStyle([('BACKGROUND', (0,0), (-1,0), colors.grey),
-#                         ('TEXTCOLOR', (0,0), (-1,0), colors.whitesmoke),
-#                         ('ALIGN', (0,0), (-1,-1), 'CENTER'),
-#                         ('FONTNAME', (0,0), (-1,0), 'Helvetica-Bold'),
-#                         ('BOTTOMPADDING', (0,0), (-1,0), 12),
-#                         ('BACKGROUND', (0,1), (-1,-1), colors.beige),
-#                         ('GRID', (0,0), (-1,-1), 1, colors.black)])
-#
-#     table.setStyle(style)
-#     doc.build([table])
-#     return response
-#
-# export_pdf.short_description = "Exportar como PDF"
