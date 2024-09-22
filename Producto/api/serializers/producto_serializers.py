0d475e24ac11d1e1e49a82729cc3a20b5a44730c
raul <74162376+raulanto@users.noreply.github.com>
2024-09-21 22:50:36 -0600

    cambios

--- Producto/api/serializers/producto_serializers.py
+++ Producto/api/serializers/producto_serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 
 from Producto.models import Producto
-
+from ..serializers.valoresProducto_serializer import ValoresProductoSerializer
+from Producto.models import ValoresProducto
 
 class ProductoSerializer(serializers.ModelSerializer):
     class Meta:
@@ -9,11 +10,25 @@ class ProductoSerializer(serializers.ModelSerializer):
         fields = ['id', 'nombre', 'descripcion', 'usuario',"fotografia"]
 
 
+
+
+
 class ProductoCreateSerializer(serializers.ModelSerializer):
+    valores = ValoresProductoSerializer(many=True, write_only=True)
     class Meta:
         model = Producto
-        fields = ['nombre', 'descripcion', 'usuario']
+        fields = ['nombre', 'descripcion', 'usuario','valores']
 
+
+    def create(self, validated_data):
+        # Extraer los datos de los valores
+        valores_data = validated_data.pop('valores')
+        # Crear el producto
+        producto = Producto.objects.create(**validated_data)
+        # Crear los valores asociados
+        for valor_data in valores_data:
+            ValoresProducto.objects.create(producto=producto, **valor_data)
+        return producto
 
 class ProductoUpdateSerializer(serializers.ModelSerializer):
     class Meta:
