4461c5829fe43a53071be6100a0fb93f91a7c82c
raul <74162376+raulanto@users.noreply.github.com>
2024-09-22 23:05:34 -0600

    cambios

--- Proceso/api/serializers/etapa_serializer.py
+++ Proceso/api/serializers/etapa_serializer.py
@@ -6,17 +6,13 @@ from ..serializers.proceso_serializer import ProcesoFkequipo
 class EtapaSerializer(serializers.ModelSerializer):
     fkProceso = ProcesoFkequipo()
 
-    # Sobrescribir el campo createdTime_at para devolver la hora en formato deseado
-    createdTime_at = serializers.SerializerMethodField()
+
 
     class Meta:
         model = Etapa
         fields = ['id', 'nombre', 'activo', 'fkProceso', 'duracion', 'created_at', 'horacreacion', 'updated_at',
                   'proceso']
 
-    def get_createdTime_at(self, obj):
-        # Formatear la hora en el formato 08:08:50
-        return obj.createdTime_at.strftime('%H:%M:%S')
 
 
 class EtapaCreateSerializer(serializers.ModelSerializer):
